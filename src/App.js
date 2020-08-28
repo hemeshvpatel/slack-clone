9d11ddcaee77ad9a7cb93a70e3b9cf576ddcaf23
hemeshvpatel <hemesh.v.patel@gmail.com>
2020-08-27 20:44:57 -0500

    header complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     //BEM naming convention (http://getbem.com/naming/)
     <div className="app">
-      <h1>Let's build a Slack Clone 🚀</h1>
+      <h1>Let's build a Slacks Clone 🚀</h1>
 
       {/* Header */}
+      <Header />
       {/* Sidebar */}
       {/* {React-Router -> Chat screen} */}
     </div>
