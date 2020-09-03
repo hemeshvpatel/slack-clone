7afef5849c214426ea6c3ba1788d2514781bb8ea
hemeshvpatel <hemesh.v.patel@gmail.com>
2020-09-03 09:57:49 -0500

    added icons and option names to sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,22 @@
 import React from "react";
 import "./Sidebar.css";
-import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
-import CreateTwoToneIcon from "@material-ui/icons/CreateTwoTone";
 import SidebarOption from "./SidebarOption";
+
+//material-ui imports
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import CreateIcon from "@material-ui/icons/Create";
 import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import AlternateEmailIcon from "@material-ui/icons/AlternateEmail";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PageviewIcon from "@material-ui/icons/Pageview";
+import MarkunreadMailboxIcon from "@material-ui/icons/MarkunreadMailbox";
+import QuestionAnswerIcon from "@material-ui/icons/QuestionAnswer";
+import GroupIcon from "@material-ui/icons/Group";
+import AppsIcon from "@material-ui/icons/Apps";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import AddIcon from "@material-ui/icons/Add";
 
 function Sidebar() {
   return (
@@ -16,10 +29,24 @@ function Sidebar() {
             Hemesh Patel
           </h3>
         </div>
-        <CreateTwoToneIcon />
+        <CreateIcon />
       </div>
       <SidebarOption Icon={InsertCommentIcon} title="Threads" />
-      <SidebarOption title="YouTube" />
+      <SidebarOption Icon={AlternateEmailIcon} title="Mentions & reactions" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Saved items" />
+      <SidebarOption Icon={PageviewIcon} title="Channel Browser" />
+      <SidebarOption Icon={MarkunreadMailboxIcon} title="All unreads" />
+      <SidebarOption Icon={QuestionAnswerIcon} title="All DMs" />
+      <SidebarOption Icon={FileCopyIcon} title="File Browser" />
+      <SidebarOption Icon={GroupIcon} title="People & User Groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+      <hr></hr>
+      <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+      <SidebarOption Icon={AddIcon} title="Add a channel" />
+      <hr></hr>
+      <SidebarOption Icon={ExpandMoreIcon} title="Direct messages" />
+      <SidebarOption Icon={AddIcon} title="Invite people" />
     </div>
   );
 }
